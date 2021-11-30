6852e3b4e50571dbc88d77406b02fad3d510a7e1
weicong xie <weicongxie@weicongdeMBP.lan>
2021-12-01 00:23:14 +0800

    comment1.1

--- mysite/courses/forms.py
+++ mysite/courses/forms.py
@@ -4,4 +4,4 @@ class NameForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Search', 'style': 'width: 300px;', 'class': 'form-control me-2'}))
 
 class CommentForm(forms.Form):
-    your_comment = forms.CharField(label='Your comment', max_length=500, widget=forms.Textarea)
+    your_comment = forms.CharField(label='Your comment', max_length=500, widget=forms.Textarea(attrs={'placeholder': 'Please enter your comments', 'class': 'form-control'}))
