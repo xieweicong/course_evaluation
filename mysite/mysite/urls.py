d073eaea0e5ea6955d57032acd595c6cb4158963
weicong xie <weicongxie@xieweicongdeMacBook-Pro.local>
2021-12-17 19:45:19 +0800

    login

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,7 +5,8 @@ from courses import views
 
 
 urlpatterns = [
-    path('', views.get_name, name='get_name'),
-    path('courses/', include('courses.urls')),
-    path('admin/', admin.site.urls),
+    path("", views.get_name, name="get_name"),
+    path("courses/", include("courses.urls")),
+    path("admin/", admin.site.urls),
+    path("user/", include("userprofile.urls")),
 ]
